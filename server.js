347f2b93f37f75d12143a6dfce8be30b72c6de31
Alon Cohen <alon@cohencodes.com>
2019-04-19 12:36:29 -0700

    prepared for deployment

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const POKEDEX = require('./pokedex.json');
-
 const app = express();
-app.use(morgan('tiny'));
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common';
+app.use(morgan(morganSetting));
+
 app.use(
   (validateBearerToken = (req, res, next) => {
     const apiToken = process.env.API_TOKEN;
     const authToken = req.get('Authorization');
-    console.log('validate bearer token middleware');
 
     if (!authToken || authToken.split(' ')[1] !== apiToken) {
       return res.status(401).json({ error: 'Unauthorized request' });
@@ -17,6 +17,16 @@ app.use(
     next();
   })
 );
+
+app.use((error, req, res, next) => {
+  let response;
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' } };
+  } else {
+    response = { error };
+  }
+  res.status(500).json(response);
+});
 
 const validTypes = [
   `Bug`,
